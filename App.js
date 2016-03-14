63baafdc5bec26e4a5446a97499c6891635d5cd9
Nilesh Deshpande <nideshp@microsoft.com>
2016-03-14 12:17:56 -0700

    State

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { title : "Default title from state" ,
+                        template : "Empty template" };
+    }
+    update(e) {
+        var newState = {};
+        newState[e.target.attributes['data-ref'].value] = e.target.value ;
+        this.setState(newState);        
+    }
     render() {
-        let title = this.props.title;
-        let template = this.props.title;
+        let title = this.state.title;
+        let template = this.state.template;
         return (
             <div>
                 <h1>{title}</h1>
+                <input type="text" data-ref="title" onChange={this.update.bind(this)}/> 
                 <h3>{template}</h3>
+                <input type="text" data-ref="template" onChange={this.update.bind(this)}/> 
             </div>
         );
     }
