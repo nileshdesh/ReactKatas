52416ec0004a645c589c95e72ab4f00a4f13366d
Nilesh Deshpande <nideshp@microsoft.com>
2016-03-14 00:35:33 -0700

    Passing simple properties

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import React from 'react';
-class App extends React.Component { 
-    render(){
-        return <h1>Hello World</h1>
+class App extends React.Component {
+    render() {
+        let title = this.props.title;
+        let template = this.props.title;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h3>{template}</h3>
+            </div>
+        );
     }
-    
+}
+
+App.propTypes = {
+    title: React.PropTypes.string.isRequired,
+    template: React.PropTypes.string.isRequired
+}
+
+App.defaultProps = {
+    title: "Default Title",
+    template: "Default Template"
 }
 
 export default App;
