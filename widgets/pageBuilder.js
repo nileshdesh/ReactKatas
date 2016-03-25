4551f87fff198c3f2dd1eb45bcd085337d8168d0
Nilesh Deshpande <nideshp@microsoft.com>
2016-03-24 17:29:17 -0700

    Fixing page builder (using JSX)

--- widgets/pageBuilder.js
+++ widgets/pageBuilder.js
@@ -7,7 +7,6 @@ class PageBuilder extends React.Component {
     }
     render() {
         var source = JSON.parse(this.props.source ? this.props.source : '{"tag": "input"}');
-        debugger
         var element = createElements(source);
 
         return element;
@@ -17,12 +16,13 @@ class PageBuilder extends React.Component {
 
 }
 
-function createElements(element) {
+function createElements(element, index = 0) {
     if (element.children) {
-        var childElements = element.children.map(function(child) {
-            return createElements(child);
+        var childElements = element.children.map(function(child, index) {
+          return createElements(child, index);
         });
-        return React.createElement(element.tag, element.attrs, childElements);
+          return (<element.tag {...element.attrs}>{childElements}</element.tag>);
+        
     }
     if (element.content)
         return (<element.tag {...element.attrs}>{element.content}</element.tag>)
